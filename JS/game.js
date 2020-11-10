98b52f96ec4e92e40266fd78d3f2323640423cc0
gavilanroland <gavilanrolando@protonmail.com>
2020-11-10 17:17:06 -0300

    week six RR Javascript eventloop

---
+++ JS/game.js
@@ -0,0 +1,35 @@
+var canvas = null,
+    ctx = null;
+    x = 50;
+    y = 50;
+    lastPress = null;
+
+document.addEventListener('keydown', function (e) {
+    lastPress = e.which;
+}, false);
+
+function paint(ctx) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+        
+    ctx.fillStyle = '#0f0';
+    ctx.fillRect(x, y, 10, 10);
+}
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    run();
+}
+function run() {
+    window.requestAnimationFrame(run);
+    act();
+    paint(ctx);
+}
+function act(){
+    x += 2;
+    if (x > canvas.width) {
+        x = 0;
+    }
+}
+window.addEventListener('load', init, false);
---
+++ README.md
@@ -0,0 +1,4 @@
+
+interactive video game
+Javascript applications, event loop events
+It is part of the practice of week 6 of RR.
